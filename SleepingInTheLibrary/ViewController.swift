5755ac9939631b3c2366dde2ab0f55699084a174
andreskwan <andreskwan@gmail.com>
2016-04-13 01:17:48 -0500

    SITL - [add method escapeParameters] - documented - learn about Optionals and unwrapping Strings

--- SleepingInTheLibrary/ViewController.swift
+++ SleepingInTheLibrary/ViewController.swift
@@ -41,12 +41,15 @@ class ViewController: UIViewController {
     // MARK: Make Network Request
     
     private func getImageFromFlickr() {
-        let urlGetGalleryPhotos = "\(Constants.Flickr.APIBaseURL)?" +
-            "\(Constants.FlickrParameterKeys.Method)=\(Constants.FlickrParameterValues.GalleryPhotosMethod)&" +
-            "\(Constants.FlickrParameterKeys.GalleryID)=\(Constants.FlickrParameterValues.GalleryID)&" +
-            "\(Constants.FlickrParameterKeys.Extras)=\(Constants.FlickrParameterValues.MediumURL)&" +
-            "\(Constants.FlickrParameterKeys.Format)=\(Constants.FlickrParameterValues.ResponseFormat)&" +
-            "\(Constants.FlickrParameterKeys.NoJSONCallback)=\(Constants.FlickrParameterValues.DisableJSONCallback)"
+        let urlParameters = [
+            Constants.FlickrParameterKeys.Method:Constants.FlickrParameterValues.GalleryPhotosMethod,
+            Constants.FlickrParameterKeys.GalleryID:Constants.FlickrParameterValues.GalleryID,
+            Constants.FlickrParameterKeys.Extras:Constants.FlickrParameterValues.MediumURL,
+            Constants.FlickrParameterKeys.Format:Constants.FlickrParameterValues.ResponseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback:Constants.FlickrParameterValues.DisableJSONCallback]
+        
+        let urlGetGalleryPhotosParams = escapedParameters(urlParameters)
+        let urlGetGalleryPhotos = "\(Constants.Flickr.APIBaseURL)" + urlGetGalleryPhotosParams
         
         // TODO: Write the network code here!
         let url = NSURL(string: urlGetGalleryPhotos)
@@ -54,5 +57,36 @@ class ViewController: UIViewController {
         print("url:" + url!.absoluteString)
         //after retrieve the image
         setUIEnabled(true)
+    }
+    
+    /// Takes a dictionary
+    ///
+    /// - parameters:
+    ///     - dictionary:
+    /// - returns: A String witch represent a list of parameters and their values needed for an API method call for a web service with only safe **ASCII** characters, each pair is separated by an ampersand, and the string begins with **?** to indicate the begining of the request
+    ///
+    /// - precondition: keys should be valid **ASCII** characters (keys represent names of the parameters of the url functions of the API of the web service)
+    ///
+    ///
+    private func escapedParameters(parameters: [String:AnyObject]) -> String {
+        //1 check if any parameter is provided
+        if parameters.isEmpty {
+            return ""
+        } else { // create and array that stores each key value pair as we format it
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters {
+                // we assume that the keys are valid **ASCII** characters
+                // values should be casted to Strings
+                let stringValue = "\(value)"
+                // escaoe it
+                // convert the string into an **ASCII** compliant version of a string
+                let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+                // append it 
+                // escapedValue needs to be unwrapped to avoid Optiona(\"characters\")
+                keyValuePairs.append(key + "=" + "\(escapedValue!)")
+            }
+            return "?\(keyValuePairs.joinWithSeparator("&"))"
+        }
     }
 }
